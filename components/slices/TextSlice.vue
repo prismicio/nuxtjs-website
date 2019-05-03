8ce6d1c79b5916dd6fe9111ae8bdda67e313e4b3
Philip Snow <philipsnow@Philips-MacBook-Pro.local>
2019-05-03 13:24:30 +0200

    css update 2 col

--- components/slices/TextSlice.vue
+++ components/slices/TextSlice.vue
@@ -12,36 +12,25 @@ export default {
 <style lang="sass" scoped>
 .text-section-2col
   -webkit-column-count: 2
-  // Chrome, Safari, Opera
   -moz-column-count: 2
-  // Firefox
   column-count: 2
   -webkit-column-gap: 40px
-  // Chrome, Safari, Opera
   -moz-column-gap: 40px
-  // Firefox
   column-gap: 40px
 
 .text-section-1col img, .text-section-2col img, .gallery img
   margin-bottom: 1rem
 
-.text-section-1col p:last-child, .text-section-2col p:last-child
+.text-section-1col p:last-child, .text-section-2col p:last-child, .content-section.text-section-2col
   margin-bottom: 0
 
 @media (max-width: 767px)
-  .content-section
-    margin-bottom: 2rem
-
   .text-section-2col
     -webkit-column-count: 1
-    // Chrome, Safari, Opera
     -moz-column-count: 1
-    // Firefox
     column-count: 1
     -webkit-column-gap: 40px
-    // Chrome, Safari, Opera
     -moz-column-gap: 40px
-    // Firefox
     column-gap: 40px
 </style>
 
