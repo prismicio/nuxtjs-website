9d2c20eb0e949f6c17bc2380d9f76dcc86f6a050
Sam Littlefair <sam.littlefair@prismic.io>
2020-10-02 13:34:02 +0200

    Update link handling with prismicLinks.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: [{ src: "~/plugins/prismicLinks", ssr: false }],
 
   /*
    ** Nuxt.js modules
